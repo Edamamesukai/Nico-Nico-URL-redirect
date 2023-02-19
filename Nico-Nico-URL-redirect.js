36c0fab6b2405b36278d3ac066dc78cf0f926e36
Edamamesukai <88695691+Edamamesukai@users.noreply.github.com>
2023-02-19 18:55:46 +0900

    初期

---
+++ Nico-Nico-URL-redirect.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Nico-Nico-URL-redirect
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  ニコニコのドメインのスマホ版表示を直します
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log("niconico domain auto changer 実行します")
+    // 動画プレイヤーがスマホ表示の場合、パソコンに直す
+    if (location.href.match("sp.nicovideo.jp")) {
+        location.href = location.href.replace("sp.nicovideo.jp", "nicovideo.jp");
+    }
+    // ニコニコ静画がスマホ表示の場合、パソコンに直す
+    else if (location.href.match("sp.seiga.nicovideo.jp")) {
+        location.href = location.href.replace("sp.seiga.nicovideo.jp", "seiga.nicovideo.jp").replace("/#!", "");
+    }
+    // ニコニコ大百科がスマホ表示の場合、パソコンに直す
+    else if (location.href.match("dic.nicovideo.jp/t/a")) {
+        location.href = location.href.replace("dic.nicovideo.jp/t/a", "dic.nicovideo.jp/a");
+    }
+})();
