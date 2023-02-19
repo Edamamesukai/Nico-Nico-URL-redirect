792563f9b67fab8e574586fb33ccca9578f9de21
Edamamesukai <88695691+Edamamesukai@users.noreply.github.com>
2023-02-19 21:02:40 +0900

    Ver.1.0.1 authorとコメントアウトを修正

--- Nico-Nico-URL-redirect.js
+++ Nico-Nico-URL-redirect.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Nico-Nico-URL-redirect
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  ニコニコのドメインのスマホ版表示を直します
-// @author       You
+// @author       Edamame_sukai
 // @match        *://sp.nicovideo.jp/*
 // @match        *://sp.seiga.nicovideo.jp/*
 // @match        *://dic.nicovideo.jp/t/a/*
@@ -15,15 +15,15 @@
 
     // Your code here...
     console.log("niconico domain auto changer 実行します")
-    // 動画プレイヤーがスマホ表示の場合、パソコンに直す
+    // 動画プレイヤーがスマホ表示の場合、パソコン表示に直す
     if (location.href.match("sp.nicovideo.jp")) {
         location.href = location.href.replace("sp.nicovideo.jp", "nicovideo.jp");
     }
-    // ニコニコ静画がスマホ表示の場合、パソコンに直す
+    // ニコニコ静画がスマホ表示の場合、パソコン表示に直す
     else if (location.href.match("sp.seiga.nicovideo.jp")) {
         location.href = location.href.replace("sp.seiga.nicovideo.jp", "seiga.nicovideo.jp").replace("/#!", "");
     }
-    // ニコニコ大百科がスマホ表示の場合、パソコンに直す
+    // ニコニコ大百科がスマホ表示の場合、パソコン表示に直す
     else if (location.href.match("dic.nicovideo.jp/t/a")) {
         location.href = location.href.replace("dic.nicovideo.jp/t/a", "dic.nicovideo.jp/a");
     }
